3b89fe14caabec1ce9c3dc5245cd9ac0794850ce
tobi <maciej.tobiasz@gmail.com>
2019-12-26 00:31:36 +0100

    second commit for TestJenkins

--- TestJenkins/Program.cs
+++ TestJenkins/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace TestJenkins
 {
+    [TestFixture]
     class Program
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine(createmessage());
+        }
+        public static string createmessage()
+        {
+            return "Hello World";
+        }
+        public void TestMethod()
+        {
+            Assert.AreEqual("Hello World", createmessage());
         }
     }
 }
